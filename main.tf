11948c1572b7ce9f77f1eb7fffaf56c17f58ff53
Kedar077 <78084220+Kedar077@users.noreply.github.com>
2024-09-05 14:22:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1" # Set your desired region
 }
-resource "aws_instance" "foo" {
+resource "aws_instance" "kedar" {
   ami = var.ami_value
   instance_type = var.instance_value
 }
